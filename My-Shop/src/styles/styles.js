4619fe6ab04221cfc5230595de066a6a84178ad2
glendetorres <glendetorres.ccs@gmail.com>
2018-02-06 14:46:13 +0800

    added styles folder and styles.js

---
+++ My-Shop/src/styles/styles.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+
+	container: {
+		backgroundColor: '#fff',
+		marginTop: 20,
+	},
+
+	button: {
+		height: 100,
+		width: 100,
+		margin: 5,
+		backgroundColor: 'transparent',
+		borderRadius: 10,
+		borderColor: '#fff',
+		borderWidth: 1,
+		marginBottom: 15,
+	},
+
+	icon: {
+		height: 98,
+		width: 98,
+		borderRadius: 10,
+	},
+
+	servantName: {
+		margin: 2,
+		textAlign: 'center',
+		fontWeight: 'bold',
+	},
+	
+})
