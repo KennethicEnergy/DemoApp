aef06ab9b71894f1d286cde4651dfacd49e699ca
neildborbon <neilborbon.ccs@gmail.com>
2018-02-09 15:18:57 +0800

    set sample image for view slider, added brandlogo.json for sample json for featured brands grid

--- My-Shop/src/components/ViewSlider.js
+++ My-Shop/src/components/ViewSlider.js
@@ -1,27 +1,16 @@
 import React from 'react';
 import { Text, View, ScrollView, TouchableOpacity, Image } from 'react-native';
 
+import ViewSliderContent from './ViewSliderContent';
+
 export default class ViewSlider extends React.Component {
 	render(){
 		return(
 			<ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-				<TouchableOpacity>
-					<View style={{width: 200, height: 120, margin: 8,}}>
-						<Image source={{uri: 'https://www.static-src.com/siva/asset/02_2018/digital-banner-cny-travel-revised.jpg'}} style={{'width': 50, 'height': 50}} />
-					</View>
-				</TouchableOpacity>
-				<TouchableOpacity>
-					<View style={{width: 200, height: 120, backgroundColor: '#0095DA', margin: 8,}} />
-				</TouchableOpacity>
-				<TouchableOpacity>
-					<View style={{width: 200, height: 120, backgroundColor: '#0095DA', margin: 8,}} />
-				</TouchableOpacity>
-				<TouchableOpacity>
-					<View style={{width: 200, height: 120, backgroundColor: '#0095DA', margin: 8,}} />
-				</TouchableOpacity>
-				<TouchableOpacity>
-					<View style={{width: 200, height: 120, backgroundColor: '#0095DA', margin: 8,}} />
-				</TouchableOpacity>
+				<ViewSliderContent />
+				<ViewSliderContent />
+				<ViewSliderContent />
+				<ViewSliderContent />
 			</ScrollView>
 		)
 	}
